8ae0eb313c4c97b2f252e686b457d040c78c79f6
baburali <baburali1910@gmail.com>
2024-07-18 05:00:53 -0700

    add changes

---
+++ app.js
@@ -0,0 +1,134 @@
+// console.log("hello if else day-3");
+
+// if-else statement
+
+// task-1 === check if the number is positive , negative or zero
+
+// let num = 2;
+
+    
+// if (num > 0) {
+//     console.log("the number is positive");
+// }else if (num < 0){
+//     console.log("the number is negative");
+// }else{
+//     console.log("zero");
+// }
+
+
+// task-2 === check if the person is eligible for vote > 18
+
+
+// let age = 19;
+
+// if (age >= 18) {
+//     console.log("you are eligible for vote");
+// }else{
+//     console.log("you are not eligible for vote");
+// }
+
+
+// task-3 === find the largest of three number by nested if-else
+
+
+// let num1 = 2;
+
+// let num2 = 12;
+
+// let num3 = 15
+// let largest;
+// if (num1 >= num2 && num1 >= num3) {
+// largest = num1;
+
+// }else if (num2 >= num1 && num2 >= num3) {
+//     largest = num2
+// }else{
+//     largest = num3
+// }
+
+
+// console.log("the largest number is " , largest);
+
+// task-4 === check the day of week based on numbers from 1-7
+
+// let daysOfWeek = 41;
+
+// switch (daysOfWeek) {
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tues");
+//         break;
+//     case 3:
+//         console.log("wed");
+//         break;
+//     case 4:
+//         console.log("thurs");
+//         break;
+//     case 5:
+//         console.log("fri");
+//         break;
+//     case 6:
+//         console.log("sat");
+//         break;
+//     case 7:
+//         console.log("sunday");
+//         break;
+
+//     default:
+//         console.log("none of mon, tues, twed, thurs, fri, sat, sun");
+//         break;
+// }
+
+
+
+// task-5 === assign a A,B,C,D,E according to score by switch case
+
+
+
+// let grade = 60;
+
+// switch (grade) {
+//     case 90:
+//         console.log("A grade");
+//         break;
+//     case 80:
+//         console.log("B grade");
+//         break;
+//     case 70:
+//         console.log("C grade");
+//         break;
+//     case 60:
+//         console.log("D grade");
+//         break;
+//     case 50:
+//         console.log("E grade");
+//         break;
+
+//     default:
+//         console.log("none of A B C D E");
+//         break;
+// }
+
+// task-6 === use ternary operator to check the num is even or odd
+
+// let num1 = 2;
+
+// let num1 = 121;
+// num1 % 2 == 0 ? console.log("even") : console.log("odd");
+
+// task-7 === check if the year is leaf year by checking if divisible by 4 and 400 and not by 100
+
+// let year = 11;
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     console.log("leap year");
+// }else{
+//     console.log("not leap year");
+// }
+
+
+
+
+
+
